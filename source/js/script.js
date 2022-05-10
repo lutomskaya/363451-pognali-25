4de6f802d25cb43b7bc2923b6f219f928352e14d
Anna <lutomskaya@gmail.com>
2022-05-10 19:23:03 +0300

    Сделала сетку и декоративные элементы главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,14 @@ const mainHeader = document.querySelector(".main-header");
 
 const mainToggle = document.querySelector(".main-header__toggle");
 
+mainHeader.classList.remove("main-header--nojs");
+
 mainToggle.addEventListener('click', function () {
-  mainHeader.classList.remove("main-header--nojs");
-  mainHeader.classList.toggle('main-header--opened');
+  if (mainHeader.classList.contains("main-header--opened")) {
+    mainHeader.classList.remove("main-header--opened");
+    mainHeader.classList.add("main-header--closed");
+  } else {
+    mainHeader.classList.remove("main-header--closed");
+    mainHeader.classList.add("main-header--opened");
+  }
 });
