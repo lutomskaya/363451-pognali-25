3b1ee0c41cb6f6b317de6f76421f26f3881db99c
Anna <lutomskaya@gmail.com>
2022-11-10 23:42:34 +0300

    Updates

--- 7/js/script.js
+++
@@ -1,8 +0,0 @@
-const mainHeader = document.querySelector(".main-header");
-
-const mainToggle = document.querySelector(".main-header__toggle");
-
-mainToggle.addEventListener('click', function () {
-  mainHeader.classList.remove("main-header--nojs");
-  mainHeader.classList.toggle('main-header--opened');
-});
